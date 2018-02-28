b2fa51be6c1831aff93c06da4c60b4e1fb3a2961
Vanessa Niro Machado <vanessa.nirom@gmail.com>
2018-02-27 22:31:43 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # ProgramacaoVisual
 Repositório de códigos de Programação Visual_02 semestre
+
+
+// clona o projeto do GitHub
+git clone https://github.com/nessaniro/ProgramacaoVisual
+
+// cria pasta ProgramacaoVisual
+cd ProgramacaoVisual/
+
+// cria pasta source
+mkdir source
+cd source
+
+// acessa a pasta de ProgramacaoVisual
+mkdir ProgramacaoVisual
+cd ProgramacaoVisual/
+
+// cria solution
+dotnet new sln 
+
+// criar e acessar WebMvc
+mkdir WebMvc
+cd WebMvc
+
+// cria mvc 
+dotnet new mvc
+
+cd ..
+
+dotnet sln ProgramacaoVisual.sln add WebMvc/WebMvc.csproj
+
+// para adicionar no GitHub
+git add .
+
+// para ver se salvou no GitHub
+git status 
+
+git config --global user.email "vanessa.nirom@gmail.com""
+git config --global user.name "Vanessa Niro Machado""
+
+git add *
+
+git commit -m "Initial commit""
+git push origin master
+. . . 
---
+++ source/ProgramacaoVisual/WebMvc/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMvc.Models;
+
+namespace WebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static IList<Pessoa> PessoaList = new List<Pessoa>
+        {
+            new Pessoa { Id = 1, Nome = "Joao"},
+            new Pessoa { Id = 2, Nome = "Juao"},
+            new Pessoa { Id = 3, Nome = "Jonh"}
+        };
+
+        public IList<Pessoa> GetPessoas()
+        {
+            return PessoaList;
+        }
+
+        public Pessoa AddPessoa(string nome)
+        {
+            try {
+                var pessoa = new Pessoa { Nome = nome };
+                pessoa.Id = PessoaList.Max(p => p.Id) + 1;
+                PessoaList.Add(pessoa);
+                return pessoa;
+            } catch{}
+
+            return null;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
